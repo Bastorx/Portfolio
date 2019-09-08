c71a62af48674c034bcb3e8dcecd1fdecaa6fdcb
Bastien CHEVALLIER <cheval_b@etna-alternance.net>
2019-09-08 18:25:17 +0200

    Fix animation

--- src/containers/page/App.tsx
+++ src/containers/page/App.tsx
@@ -1,15 +1,16 @@
 import { h, Component } from "preact";
+import { contentWayPoint } from "../../helpers";
 
 import {
   About,
-  Work,
+  // Work,
   Header,
   Resume,
-  Features,
+  // Features,
   Skills,
-  HireMe,
-  Blog,
-  ContactForm
+  HireMe
+  // Blog,
+  // ContactForm
 } from "..";
 
 export class App extends Component {
@@ -17,43 +18,7 @@ export class App extends Component {
     $(window).stellar();
 
     // To fix
-
-    // let i = 0;
-    // $(".animate-box").waypoint(
-    //   function(direction: any) {
-    //     if (
-    //       direction === "down" &&
-    //       !$(this.element).hasClass("animated-fast")
-    //     ) {
-    //       i++;
-    //       $(this.element).addClass("item-animate");
-    //       setTimeout(function() {
-    //         $("body .animate-box.item-animate").each(function(k: any) {
-    //           const el = $(this);
-    //           setTimeout(
-    //             function() {
-    //               const effect = el.data("animate-effect");
-    //               if (effect === "fadeIn") {
-    //                 el.addClass("fadeIn animated-fast");
-    //               } else if (effect === "fadeInLeft") {
-    //                 el.addClass("fadeInLeft animated-fast");
-    //               } else if (effect === "fadeInRight") {
-    //                 el.addClass("fadeInRight animated-fast");
-    //               } else {
-    //                 el.addClass("fadeInUp animated-fast");
-    //               }
-
-    //               el.removeClass("item-animate");
-    //             },
-    //             k * 100,
-    //             "easeInOutExpo"
-    //           );
-    //         });
-    //       }, 50);
-    //     }
-    //   },
-    //   { offset: "85%" }
-    // );
+    contentWayPoint();
   }
   public render() {
     return (
---
+++ src/helpers/contentWayPoint.ts
@@ -0,0 +1,24 @@
+export const contentWayPoint = () => {
+  $(".animate-box").waypoint(
+    function(direction: any) {
+      if (direction === "down" && !$(this.element).hasClass("animation-end")) {
+        $(this.element).addClass("animated");
+        $("body .animate-box.animated").each(function(k: any) {
+          const el = $(this);
+          setTimeout(() => {
+            const effect = el.data("animate-effect");
+            this.classList.add(effect || "fadeInUp", "faster");
+
+            const handleAnimationEnd = () => {
+              this.classList.remove("animated", effect || "fadeInUp", "faster");
+              this.classList.add("animation-end");
+              this.removeEventListener("animationend", handleAnimationEnd);
+            };
+            this.addEventListener("animationend", handleAnimationEnd);
+          }, k * 100);
+        });
+      }
+    },
+    { offset: "85%" }
+  );
+};
