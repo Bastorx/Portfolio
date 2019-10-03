977dd82d7c086558df05945e692148c35e2b124c
Bastien CHEVALLIER <cheval_b@etna-alternance.net>
2019-10-03 23:18:48 +0200

    New 404 + New logo

---
+++ public/component---src-pages-404-tsx-94fef84263951036df3f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{245:function(e,t,u){"use strict";var n=this&&this.__importDefault||function(e){return e&&e.__esModule?e:{default:e}};Object.defineProperty(t,"__esModule",{value:!0});var l=n(u(0)),a=n(u(243));t.default=function(){return l.default.createElement(a.default,null,l.default.createElement("h1",null,"NOT FOUND"),l.default.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-94fef84263951036df3f.js.map
